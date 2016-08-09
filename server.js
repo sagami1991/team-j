53f5f1be90a4a774631e3306e7a2a023548b9012
hoge <hoge@hoge>
2016-08-10 01:48:10 +0900

    no message

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-var express = require('express');
+const express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
@@ -10,43 +10,43 @@ app.listen(app.get('port'), function() {
 });
 
 
-const WebSocketServer = require('ws').Server;
-const wss = new WebSocketServer({ port: 8081 });
-const WSResType = {
-	error: 0,
-	initlog: 1,
-	log: 2
-};
-const dateFormat = require('dateformat');
-let chatLog = [];
-wss.on('connection', (ws) => {
+// const WebSocketServer = require('ws').Server;
+// const wss = new WebSocketServer({ port: 8081 });
+// const WSResType = {
+// 	error: 0,
+// 	initlog: 1,
+// 	log: 2
+// };
+// const dateFormat = require('dateformat');
+// let chatLog = [];
+// wss.on('connection', (ws) => {
 
-	ws.send(JSON.stringify({
-		type: WSResType.initlog,
-		value: chatLog
-	}));
-	ws.on('message', (data, flags) => {
-		if(!flags.binary && data.length > 20){
-			ws.send(JSON.stringify({
-				type: WSResType.error,
-				value: "20文字以上は入力できません"
-			}));
-		}
-		if(chatLog.length >= 10) chatLog.shift();
-		const log = {
-			msg: data,
-			date: dateFormat(new Date(),"m/dd HH:MM")
-		};
-		chatLog.push(log);
-		wss.clients.forEach(ws => {
-			ws.send(JSON.stringify({
-				type: WSResType.log,
-				value: log
-			}));
-		})
-	});
+// 	ws.send(JSON.stringify({
+// 		type: WSResType.initlog,
+// 		value: chatLog
+// 	}));
+// 	ws.on('message', (data, flags) => {
+// 		if(!flags.binary && data.length > 20){
+// 			ws.send(JSON.stringify({
+// 				type: WSResType.error,
+// 				value: "20文字以上は入力できません"
+// 			}));
+// 		}
+// 		if(chatLog.length >= 10) chatLog.shift();
+// 		const log = {
+// 			msg: data,
+// 			date: dateFormat(new Date(),"m/dd HH:MM")
+// 		};
+// 		chatLog.push(log);
+// 		wss.clients.forEach(ws => {
+// 			ws.send(JSON.stringify({
+// 				type: WSResType.log,
+// 				value: log
+// 			}));
+// 		})
+// 	});
 
-	ws.on("close", (code) => {
-	})
+// 	ws.on("close", (code) => {
+// 	})
 
-});
+// });
